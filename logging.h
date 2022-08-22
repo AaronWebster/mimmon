7e85fe77825cbed1a93f73e2954c3f85594d10bf
Aaron Webster <webstera@google.com>
2022-08-22 12:51:31 -0700

    Skeleton message.

--- logging.h
+++ logging.h
@@ -5,25 +5,25 @@
 #include <iostream>
 #include <ostream>
 
-#define CHECK(condition)                                                       \
-  do {                                                                         \
-    if (!(condition)) {                                                        \
-      std::cerr << __builtin_FILE() << ":" << __builtin_LINE()                 \
-                << "] Check failed: " << #condition << std::endl;              \
-      std::abort();                                                            \
-    }                                                                          \
+#define CHECK(condition)                                          \
+  do {                                                            \
+    if (!(condition)) {                                           \
+      std::cerr << __builtin_FILE() << ":" << __builtin_LINE()    \
+                << "] Check failed: " << #condition << std::endl; \
+      std::abort();                                               \
+    }                                                             \
   } while (0)
 
-#define PCHECK(condition)                                                      \
-  do {                                                                         \
-    if (!(condition)) {                                                        \
-      const auto saved_errno = errno;                                          \
-      std::cerr << __builtin_FILE() << ":" << __builtin_LINE()                 \
-                << "] Check failed: " << #condition << " ("                    \
-                << std::strerror(saved_errno) << " [" << saved_errno << "])"   \
-                << std::endl;                                                  \
-      std::abort();                                                            \
-    }                                                                          \
+#define PCHECK(condition)                                                    \
+  do {                                                                       \
+    if (!(condition)) {                                                      \
+      const auto saved_errno = errno;                                        \
+      std::cerr << __builtin_FILE() << ":" << __builtin_LINE()               \
+                << "] Check failed: " << #condition << " ("                  \
+                << std::strerror(saved_errno) << " [" << saved_errno << "])" \
+                << std::endl;                                                \
+      std::abort();                                                          \
+    }                                                                        \
   } while (0)
 
-#endif // COM_WEBSTER_MIMMON_LOGGING_H_
+#endif  // COM_WEBSTER_MIMMON_LOGGING_H_
